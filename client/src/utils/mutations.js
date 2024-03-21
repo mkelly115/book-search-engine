602a513ec04668e2060b613e46753aaf2d762419
michael <mikek222@gmail.com>
2024-03-21 23:00:56 +0300

    altered mutations to delete book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,66 +1,69 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
+// Mutation to log in
 export const LOGIN_USER = gql`
-  mutation LoginUser($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
         username
-        email
       }
     }
   }
 `;
 
+// Mutation to add a new user
 export const ADD_USER = gql`
-  mutation AddUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
         username
-        email
       }
     }
   }
 `;
 
+// Mutation to save a book
 export const SAVE_BOOK = gql`
-  mutation SaveBook($input: BookInput!) {
+  mutation saveBook($input: BookInput!) {
     saveBook(input: $input) {
       _id
       username
       email
-      bookCount
       savedBooks {
-        bookId
+        _id
         authors
         description
-        title
+        bookId
         image
         link
+        title
       }
+      bookCount
     }
   }
 `;
 
-// Define the REMOVE_BOOK mutation
+// Mutation to remove a book
 export const REMOVE_BOOK = gql`
-  mutation RemoveBook($bookId: ID!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
       _id
       username
       email
-      bookCount
       savedBooks {
-        bookId
+        _id
         authors
         description
-        title
+        bookId
         image
         link
+        title
       }
+      bookCount
     }
   }
 `;
